f810122464c8ead988da9bc050d6f70c3cee2684
Renato Domingues <renatodomingues09@hotmail.com>
2022-04-13 16:34:04 -0300

    update_v4.4

--- defs.py
+++ defs.py
@@ -1,26 +1,26 @@
+# ------------------------------
+# Arquivo defs.py
+# ------------------------------
+
 import time
-import geopy.geocoders
 from geopy.geocoders import Nominatim
-import certifi
-import ssl
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-geopy.geocoders.options.default_ssl_context = ctx
+geolocator = Nominatim( user_agent='geopiExercises' )
 
-geolocator = Nominatim( user_agent='geopyExercises')
+def get_longlat( x ):
+   index, row = x
+   time.sleep(1)
+   response = geolocator.reverse( row['query'] )
+   address = response.raw['address']
 
-def get_data(x):
-    index, row = x
-   
-    #API request
-    response = geolocator.reverse( row['latlong'] )
-    address = response.raw['address']
+   try:
+       place_id = response.raw['place_id'] if 'place_id' in response.raw else 'NA'
+       osm_type = response.raw['osm_type'] if 'osm_type' in response.raw else 'NA'
 
-    place_id = response.raw['place_id'] if 'place_id' in response.raw else 'NA'
-    osm_type = response.raw['osm_type'] if 'osm_type' in response.raw else 'NA'
-    country = address['country'] if 'country' in address else 'NA'
-    country_code = address['country_code'] if 'country_code' in address else 'NA'
+       country = address['country'] if 'country' in address else 'NA'
+       country_code = address['country_code'] if 'country_code' in address else 'NA'
 
-    return place_id, osm_type, country, country_code
+       return place_id, osm_type, country, country_code
+
+   except:
+       return None, None, None, None
